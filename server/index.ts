18092b4088cdd9eb82c1ca7adbe7f34b293ec831
timmyichen <timmy.i.chen@gmail.com>
2023-03-11 14:18:17 -0800

    try/catch in server index

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 import 'dotenv-flow/config';
+import log from './lib/log';
 import Server from './server';
 
 // this code runs the express server
 async function run() {
-  const server = new Server();
-  await server.init();
-  await server.start();
+  try {
+    const server = new Server();
+    await server.init();
+    await server.start();
+  } catch (err) {
+    log('failed to start', err)
+    throw err
+  }
 }
 
 run();
