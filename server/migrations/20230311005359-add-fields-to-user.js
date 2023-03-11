92c2d07d8e6f832f51a1143407ac8d992cb63654
Matthew Bozin <mbozin94@gmail.com>
2023-03-10 17:21:07 -0800

    Create migration for new user fields.

---
+++ server/migrations/20230311005359-add-fields-to-user.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("users", "discord_name", {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    });
+    await queryInterface.addColumn("users", "bio", {
+      type: Sequelize.STRING(1000),
+      allowNull: false,
+      defaultValue: "",
+    });
+    await queryInterface.addColumn("users", "twitter_url", {
+      type: Sequelize.STRING(200),
+      allowNull: false,
+      defaultValue: "",
+    });
+    await queryInterface.addColumn("users", "linkedin_url", {
+      type: Sequelize.STRING(200),
+      allowNull: false,
+      defaultValue: "",
+    });
+    await queryInterface.addColumn("users", "github_username", {
+      type: Sequelize.STRING(200),
+      allowNull: false,
+      defaultValue: "",
+    });
+    await queryInterface.addColumn("users", "website", {
+      type: Sequelize.STRING(500),
+      allowNull: false,
+      defaultValue: "",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return [
+      await queryInterface.removeColumn("users", "discord_name"),
+      await queryInterface.removeColumn("users", "bio"),
+      await queryInterface.removeColumn("users", "twitter_url"),
+      await queryInterface.removeColumn("users", "linkedin_url"),
+      await queryInterface.removeColumn("users", "github_username"),
+      await queryInterface.removeColumn("users", "website"),
+    ];
+  },
+};
