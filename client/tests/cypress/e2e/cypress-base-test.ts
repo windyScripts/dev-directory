3ac40dfedcda212d4acbf49a06296a7c0107b93b
aravind <uaravindshenoy@gmail.com>
2023-03-16 18:58:18 +0530

    for #15 corrected formatting errors, removed comments, added new lines at the end of code.

--- client/tests/cypress/e2e/cypress-base-test.ts
+++ client/tests/cypress/e2e/cypress-base-test.ts
@@ -1,17 +1,12 @@
 /// <reference types="cypress" />
-// This allows intellisense to only include definitions from cypress.
 
 describe('Open browser and check if login button exists.', () => {
-
   beforeEach(() => {
   cy.visit('http://localhost:3000');
 })
 
 it('Checks if there is an anchor with the text login',() => {
   cy.get('a').contains('Log in').should('have.length',1)
-})
-
-
-})
+})})
 
 export {};
