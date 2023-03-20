95c6756eaa579842b83fe87f8e6628a5520d8b68
uaravindshenoy <uaravindshenoy@gmail.com>
2023-03-20 19:33:53 +0530

    #15 fixed eslint errors

--- client/tests/cypress/e2e/cypress-base-test.ts
+++ client/tests/cypress/e2e/cypress-base-test.ts
@@ -2,11 +2,11 @@
 
 describe('Open browser and check if login button exists.', () => {
   beforeEach(() => {
-  cy.visit('http://localhost:3000');
-})
+    cy.visit('http://localhost:3000');
+  });
 
-it('Checks if there is an anchor with the text login',() => {
-  cy.get('a').contains('Log in').should('have.length',1)
-})})
+  it('Checks if there is an anchor with the text login',() => {
+    cy.get('a').contains('Log in').should('have.length',1);
+  });});
 
 export {};
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 export default defineConfig({
-e2e: {
-  setupNodeEvents(on, config) {
-   // implement node event listeners here
+  e2e: {
+
+    supportFile: './client/tests/cypress/support/e2e.ts',
+    specPattern:'./client/tests/cypress/e2e',
   },
-  supportFile: "./client/tests/cypress/support/e2e.ts",
-  specPattern:"./client/tests/cypress/e2e"
-},
-fixturesFolder: "./client/tests/cypress/fixtures",
-screenshotsFolder: "./client/tests/cypress/screenshots",
-videosFolder: "./client/tests/cypress/videos",
-downloadsFolder: "./client/tests/cypress/downloads",
-fileServerFolder:"./"
+  fixturesFolder: './client/tests/cypress/fixtures',
+  screenshotsFolder: './client/tests/cypress/screenshots',
+  videosFolder: './client/tests/cypress/videos',
+  downloadsFolder: './client/tests/cypress/downloads',
+  fileServerFolder:'./',
 });
