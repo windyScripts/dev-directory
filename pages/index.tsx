4641da906f4070145afe24f56ddf221882aa667f
uaravindshenoy <uaravindshenoy@gmail.com>
2023-03-25 17:58:26 +0530

    #33 Updated get user request route on client side, added user id as part of request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { Box, Button, Container, Typography } from '@mui/material';
 import { NextPage } from 'next';
 import React from 'react';
+import jwt from 'jsonwebtoken';
+import Cookies from 'universal-cookie';
 
 import createAxiosInstance from 'client/lib/axios';
 import { getDiscordOauthUrl } from 'client/lib/oauth';
+import { AUTH_COOKIE_NAME } from 'shared/constants';
 
 interface Props {
   isAuthed: boolean;
@@ -43,8 +46,12 @@ const Index: NextPage<Props> = (props) => {
 
 Index.getInitialProps = async ({ req }) => {
   try {
+    const cookies = new Cookies(req?.headers.cookie);
+    const token = cookies.get(AUTH_COOKIE_NAME);
+    const payload = jwt.decode(token) as jwt.JwtPayload;
+
     const axios = createAxiosInstance(req);
-    await axios.get('/api/users/');
+    await axios.get('/api/users/' + payload.user_id);
 
     return { isAuthed: true };
   } catch (error) {
