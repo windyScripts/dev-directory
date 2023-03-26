6d1c5e0220e082a30c8e055f0d61e365e92f4bcf
calebco <calebcohen@hotmail.com>
2023-03-26 10:40:42 -0500

    merge conflicts

--- server/api/users/index.ts
+++ server/api/users/index.ts
@@ -1,6 +1,10 @@
 import { AsyncRouter } from 'express-async-router';
 
+<<<<<<< HEAD
 import { getUsers, getUserById } from './users.controller';
+=======
+import { getUserById } from './users.controller';
+>>>>>>> 4d31cbc (#33 Remove endpoint for getting a user's profile (#64))
 
 const userRouter = AsyncRouter();
 
