b057953fbbcfd1afe719d9e9dc7288b588af40fa
jordan Moldovan <jmoldyvan@gmail.com>
2023-03-27 15:45:27 -0700

    s

---
+++ server/config/config.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { cleanEnv, str } = require('envalid');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv-flow').config();
+
+const env = cleanEnv(process.env, {
+  DB_PORT: str(),
+  DB_USER: str(),
+  DB_HOST: str(),
+  DB_NAME: str(),
+  DB_PASSWORD: str(),
+  DATABASE_URL: str(),
+});
+
+const dialectOptions = env.isProd ? {
+  ssl: {
+    rejectUnauthorized: false,
+  },
+} : undefined;
+
+const creds = {
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  host: env.DB_HOST,
+  database: env.DB_NAME,
+  port: env.DB_PORT,
+  dialect: 'postgresql',
+  dialectOptions,
+};
+
+module.exports = {
+  development: creds,
+  test: creds,
+  production: creds,
+};
---
+++ server/lib/db.ts
@@ -0,0 +1,63 @@
+import 'dotenv-flow/config';
+import { cleanEnv, str, num, bool } from 'envalid';
+import { Sequelize, Dialect  } from 'sequelize';
+
+import log from './log';
+
+const env = cleanEnv(process.env, {
+  DB_HOST: str(),
+  DB_NAME: str(),
+  DB_USER: str(),
+  DB_PASSWORD: str(),
+  DB_PORT: num(),
+  DB_LOGGING: bool({ default: false }),
+  DATABASE_URL: str(),
+});
+
+const dialectOptions = env.isProd ? {
+  ssl: {
+    rejectUnauthorized: false,
+  },
+} : undefined;
+
+const commonOptions = {
+  dialect: 'postgres' as Dialect,
+  logging: env.DB_LOGGING,
+};
+
+// Database object modeling mongoDB data
+export class Database {
+  dbName: string;
+  sequelize: Sequelize;
+
+  constructor(database = env.DB_NAME) {
+    this.dbName = database;
+
+    if (env.isProd) {
+      this.sequelize = new Sequelize(env.DATABASE_URL, {
+        dialectOptions,
+        ...commonOptions,
+      });
+    } else {
+      this.sequelize = new Sequelize({
+        port: env.DB_PORT,
+        username: env.DB_USER,
+        password: env.DB_PASSWORD,
+        host: env.DB_HOST,
+        database: database,
+        ...commonOptions,
+      });
+    }
+  }
+
+  // Connect to the postgres db
+  async connect() {
+    // connnect
+    await this.sequelize.authenticate();
+    log('Connected to db! :)');
+  }
+}
+
+const Db = new Database();
+
+export default Db;
