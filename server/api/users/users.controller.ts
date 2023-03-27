1389d668f00a5ccab8876e6431f1060f3774e569
Matthew Bozin <mbozin94@gmail.com>
2023-03-27 12:12:11 -0700

    Replaced code in users.controller with lodash .pick(). Moved Updatablefields type.

--- server/api/users/users.controller.ts
+++ server/api/users/users.controller.ts
@@ -18,14 +18,6 @@ export const getCurrentUser: RequestHandler<void, ClientUser> = (req, res) => {
   res.json(filteredUser);
 };
 
-interface UpdatableFields {
-  bio?: string;
-  twitter_username?: string;
-  linkedin_url?: string;
-  github_username?: string;
-  website?: string;
-}
-
 export const getUserById: RequestHandler<{ id: string }, UserProfile> = async (req, res) => {
   const user = await User.findByPk(req.params.id, {
     attributes: [
@@ -47,27 +39,27 @@ export const getUserById: RequestHandler<{ id: string }, UserProfile> = async (r
   res.json(user);
 };
 
+interface UpdatableFields {
+  bio?: string;
+  twitter_username?: string;
+  linkedin_url?: string;
+  github_username?: string;
+  website?: string;
+}
+
 export const updateUserById: RequestHandler<{ id: string }, string, UpdatableFields> = async (req, res) => {
 
   if (Object.keys(req.body).length === 0) {
     throw new BadRequestError('Request body should not be an empty object.');
   }
 
-  const acceptableFields = [
+  const toUpdate = _.pick(req.body, [
     'bio',
     'twitter_username',
     'linkedin_url',
     'github_username',
     'website',
-  ];
-
-  const toUpdate: UpdatableFields = {};
-
-  acceptableFields.forEach((field) => {
-    if (field in req.body) {
-      toUpdate[field as keyof UpdatableFields] = req.body[field as keyof UpdatableFields];
-    }
-  });
+  ]);
 
   const updated = await User.update(toUpdate, {
     where: {
