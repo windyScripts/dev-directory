22afe239e6827e1e765d77dabe6a5aee56df0fea
Matthew Bozin <111899522+MatthewBozin@users.noreply.github.com>
2023-03-28 15:39:50 -0700

    #10 api endpoint update user profile (#50)
    * Created initial patch endpoint and basic working acceptance tests.
    
    * Built out user id validation and tests.
    
    * Added middleware to remove markup from properties of req.body. Added new acceptance test for removing markup.
    
    * Created requireSameId middleware. Added field validation to updateUserById.
    
    * Small optimizations to update user endpoint.
    
    * Removed markup removal test from update user endpoint tests, and created middleware test instead.
    
    * Added check for empty objects in users.controller. Added acceptance test for that check, and improved existing tests.
    
    * Installed he. Uninstalled isomorphic-dompurify. Changed, renamed, and updated unit tests for middleware for encoding unsafe characters in input fields.
    
    * Replaced code in users.controller with lodash .pick(). Moved Updatablefields type.
    
    * Updated PATCH tests in users.test.ts with new createUser() helper function.
    
    * Removed unused getCurrentUser reference in users/index.ts.
    
    * Updated PATCH /id endpoint test to login a user before comparing ids.
    
    * Replaced check for empty object in PATCH controller with check for any updatable fields. Replaced test as well.
    
    * Added new test to PATCH endpoint if request body is blank. Simplified logic in users.controller that detects if req.body has updatable fields.

--- server/api/users/users.controller.ts
+++ server/api/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import { NotFoundError } from 'express-response-errors';
+import { BadRequestError, NotFoundError } from 'express-response-errors';
 import _ from 'lodash';
 
 import { User } from 'server/models';
@@ -7,7 +7,7 @@ import { UserProfile } from 'server/types/User';
 
 type ClientUser = Pick<User, 'id' | 'discord_user_id'>
 
-export const getCurrentUser: RequestHandler<void, ClientUser>  = (req, res) => {
+export const getCurrentUser: RequestHandler<void, ClientUser> = (req, res) => {
   // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
   const filteredUser = _.pick(req.user!, [
     'id',
@@ -18,7 +18,7 @@ export const getCurrentUser: RequestHandler<void, ClientUser>  = (req, res) => {
   res.json(filteredUser);
 };
 
-export const getUserById: RequestHandler<{id: string}, UserProfile> = async (req, res) => {
+export const getUserById: RequestHandler<{ id: string }, UserProfile> = async (req, res) => {
   const user = await User.findByPk(req.params.id, {
     attributes: [
       'id',
@@ -37,4 +37,41 @@ export const getUserById: RequestHandler<{id: string}, UserProfile> = async (req
   }
 
   res.json(user);
+};
+
+interface UpdatableFields {
+  bio?: string;
+  twitter_username?: string;
+  linkedin_url?: string;
+  github_username?: string;
+  website?: string;
+}
+
+export const updateUserById: RequestHandler<{ id: string }, string, UpdatableFields> = async (req, res) => {
+
+  const fieldsToUpdate = [
+    'bio',
+    'twitter_username',
+    'linkedin_url',
+    'github_username',
+    'website',
+  ];
+
+  const toUpdate = _.pick(req.body, fieldsToUpdate);
+
+  if (Object.keys(toUpdate).length === 0) {
+    throw new BadRequestError('Request body should contain at least one updatable field.');
+  }
+
+  const updated = await User.update(toUpdate, {
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (updated[0] === 0) {
+    throw new NotFoundError('User not found');
+  }
+
+  res.sendStatus(200);
 };
---
+++ server/middleware/users.ts
@@ -0,0 +1,23 @@
+import { ForbiddenError } from 'express-response-errors';
+import * as he from 'he';
+
+import { AnyRequestHandler } from 'server/types/express';
+
+export const requireSameId: AnyRequestHandler = async (req, res, next) => {
+  if (req.user.id.toString() !== req.params.id) {
+    throw new ForbiddenError('User can only make changes to their own profile');
+  }
+
+  next();
+};
+
+export const sanitize: AnyRequestHandler = async (req, res, next) => {
+  const fields = Object.keys(req.body);
+  fields.forEach((field) => {
+    req.body[field] = he.encode(req.body[field], {
+      useNamedReferences: true,
+    });
+  });
+
+  next();
+};
