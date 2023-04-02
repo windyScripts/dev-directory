270b6ffe9fa92e1492deb7b27b18763571ecddbd
uaravindshenoy <uaravindshenoy@gmail.com>
2023-04-02 15:33:21 +0530

    Added GHA file Altered .gitignore to exclude cypress screenshots and videos folders from commits

--- jest.config.ts
+++ jest.config.ts
@@ -6,8 +6,6 @@ const config: Config.InitialOptions = {
   testEnvironment: 'node',
   testMatch: [
     '/dist/**/__tests__/**/*.+(ts|tsx|js)',
-    //'**/__tests__/**/*.+(ts|tsx|js)',
-    //'**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', { diagnostics: false }],
