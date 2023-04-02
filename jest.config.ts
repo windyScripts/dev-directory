af0e92c62c34b3c69ab6b37b17792b43da2bdc30
uaravindshenoy <uaravindshenoy@gmail.com>
2023-04-02 17:39:33 +0530

    Changed jest config

--- jest.config.ts
+++ jest.config.ts
@@ -5,8 +5,8 @@ const config: Config.InitialOptions = {
   moduleDirectories: ['node_modules'],
   testEnvironment: 'node',
   testMatch: [
-    '**/dist/**/__tests__/**/*.+(ts|tsx|js)',
-    '**/dist/**/?(*.)+(spec|test).+(ts|tsx|js)',
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', { diagnostics: false }],
