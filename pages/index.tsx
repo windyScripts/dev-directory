9bab224dd199dad3d31d3af37993024c6b7cf2a3
Ryan <ryanmahara@gmail.com>
2023-04-05 11:56:57 -0500

    remove all onboarding logic from index.tsx as it was in the wrong place

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Box, Button, Container, Typography } from '@mui/material';
 import jwt from 'jsonwebtoken';
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
 import React from 'react';
 import Cookies from 'universal-cookie';
 
@@ -11,35 +10,15 @@ import { AUTH_COOKIE_NAME } from 'shared/constants';
 
 interface Props {
   isAuthed: boolean;
-  seenOnBoarding: boolean;
 }
 
 const Index: NextPage<Props> = props => {
   const [isAuthed, setIsAuthed] = React.useState(props.isAuthed);
-  const [seenOnBoarding, setSeenOnBoarding] = React.useState(props.seenOnBoarding);
-  const router = useRouter();
-
-  // to avoid this error:
-  // During Pre-rendering (SSR or SSG) you tried to access a router method push, replace, back, which is not supported.
-  React.useEffect(() => {
-    const { query } = router;
-    if (query.seenOnBoarding === 'true') {
-      setSeenOnBoarding(true);
-      // reset query parameters once user gets back to home page
-      router.replace({
-        pathname: '/',
-        query: {},
-      });
-    } else if (!seenOnBoarding && isAuthed) {
-      router.push('/onBoarding');
-    }
-  }, []);
 
   const onLogout = async () => {
     const axios = createAxiosInstance();
     await axios.post('/api/auth/logout');
     setIsAuthed(false);
-    setSeenOnBoarding(false);
   };
 
   return (
@@ -72,9 +51,9 @@ Index.getInitialProps = async ({ req }) => {
     const axios = createAxiosInstance(req);
     await axios.get('/api/users/' + payload.user_id);
 
-    return { isAuthed: true, seenOnBoarding: false };
+    return { isAuthed: true };
   } catch (error) {
-    return { isAuthed: false, seenOnBoarding: false };
+    return { isAuthed: false };
   }
 };
 
