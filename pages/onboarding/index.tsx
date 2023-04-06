8bd91e95168e09ef81e830563174eb96e1d6a481
Ryan <ryanmahara@gmail.com>
2023-04-06 10:17:30 -0500

    removed onClick handler to use Link component instead

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -1,22 +1,19 @@
 import { Box, Button, Container, Typography } from '@mui/material';
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const Index: NextPage = () => {
-  const router = useRouter();
-  const onSkip = () => {
-    router.push('/');
-  };
-
   return (
     <Container maxWidth="lg" className="flex justify-between pt-4">
       <Typography variant="h1" className="text-3xl">
         Onboarding
       </Typography>
       <Box className="flex items-center gap-4">
-        <Button variant="contained" color="secondary" onClick={onSkip}>
-          Skip
-        </Button>
+        <Link href="/" passHref>
+          <Button variant="contained" color="secondary">
+            Skip
+          </Button>
+        </Link>
       </Box>
     </Container>
   );
