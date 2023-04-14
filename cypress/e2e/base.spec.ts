94261d8ed6edf58174f4d091108d058d0b1fea67
jordan Moldovan <jmoldyvan@gmail.com>
2023-04-14 15:42:51 -0700

    add e2e testing

--- cypress/e2e/base.spec.ts
+++ cypress/e2e/base.spec.ts
@@ -20,4 +20,27 @@ describe('Visit directory page and verify users exists', () => {
   });
 });
 
+describe('Onboarding Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/onboarding');
+  });
+
+  it('displays the correct heading', () => {
+    cy.contains('h1', 'Onboarding');
+  });
+
+  it('fills out the form successfully', () => {
+    cy.get('form').within(() => {
+      cy.get('textarea[name="bio"]').type('Lorem ipsum dolor sit amet.');
+      cy.get('input[name="twitter_username"]').type('test_twitter');
+      cy.get('input[name="linkedin_url"]').type('https://www.linkedin.com/in/test_linkedin');
+      cy.get('input[name="github_username"]').type('test_github');
+      cy.get('input[name="website"]').type('https://www.testwebsite.com');
+      cy.visit('http://localhost:3000/');
+    });
+
+    cy.url().should('equal', 'http://localhost:3000/');
+  });
+});
+
 export {};
