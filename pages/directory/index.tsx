c11f459e1e85a49034d86282a82cbee39cef5862
Ryan <ryanmahara@gmail.com>
2023-04-17 12:55:28 -0500

    add simiple UI for /directory user cards to test infinite scoll

--- pages/directory/index.tsx
+++ pages/directory/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, List, ListItem, Typography } from '@mui/material';
+import { Avatar, Box, Container, Grid, List, ListItem, Typography } from '@mui/material';
 import { NextPage } from 'next';
 import React from 'react';
 
@@ -18,13 +18,58 @@ const Directory: NextPage<{ users: any[]; error: string }> = props => {
         <Box>
           {error && <Typography>{error}</Typography>}
           <List>
-            {users.map(user => (
-              <ListItem key={user.id}>{user.discord_name}</ListItem>
-            ))}
+            {users.map(user => {
+              return (
+                <ListItem key={user.id}>
+                  <ProfileCard user={user} />
+                </ListItem>
+              );
+            })}
           </List>
         </Box>
       </Box>
     </Container>
+  );
+};
+
+// this is a copy of UserObject in server\types\User.ts (temporary fix)
+interface UserObj {
+  id: number;
+  discord_user_id: string;
+  discord_name: string;
+  bio: string;
+  twitter_username: string;
+  linkedin_url: string;
+  github_username: string;
+  website: string;
+}
+
+//TODO: fix types later
+const ProfileCard: React.FC<{ user: UserObj }> = props => {
+  const { user } = props;
+  // grid inspired by https://mui.com/material-ui/react-grid/#complex-grid
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'hsla(0, 0%, 85%, 1)',
+        padding: '16px 25px',
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item>
+          <Avatar sx={{ width: 67, height: 67 }}>{user.discord_name.slice(0, 1)}</Avatar>
+        </Grid>
+        <Grid item container xs sm direction="column">
+          <Grid item xs>
+            <Typography variant="subtitle1">{user.discord_name}</Typography>
+          </Grid>
+          <Grid item xs>
+            <Typography variant="subtitle1">Somewhere</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Typography variant="body1">{user.bio}</Typography>
+    </Box>
   );
 };
 
