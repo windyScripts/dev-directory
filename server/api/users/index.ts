6325dab61b7fc1910998fbfd87809cb6d927227f
Devin Lane <devin.c.lane@gmail.com>
2023-04-17 16:48:45 -0700

    import getUserFlags in users/index.ts

--- server/api/users/index.ts
+++ server/api/users/index.ts
@@ -3,7 +3,7 @@ import { AsyncRouter } from 'express-async-router';
 import { requireUser } from 'server/middleware/auth';
 import { requireSameId, sanitize } from 'server/middleware/users';
 
-import { getUserById, getUsers, updateUserById } from './users.controller';
+import { getUserById, getUsers, updateUserById, getUserFlags } from './users.controller';
 
 const userRouter = AsyncRouter();
 
@@ -12,5 +12,7 @@ userRouter.get('/:id', getUserById);
 userRouter.get('/', getUsers);
 
 userRouter.patch('/:id', requireUser, requireSameId, sanitize, updateUserById);
+
+userRouter.get('/:id/flags', requireUser, requireSameId, getUserFlags);
 
 export default userRouter;
