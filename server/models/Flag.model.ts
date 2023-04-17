1e82e5f8f179a5a525d6b3da820010d3fec4e5ae
Devin Lane <devin.c.lane@gmail.com>
2023-04-17 16:13:07 -0700

    add flag model

---
+++ server/models/Flag.model.ts
@@ -0,0 +1,37 @@
+import { Model, DataTypes } from 'sequelize';
+
+import Db from 'server/lib/db';
+
+class Flag extends Model {
+  public id!: number;
+  public user_id!: number;
+  public flag_name!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Flag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    flag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: Db.sequelize,
+    modelName: 'Flag',
+    tableName: 'flags',
+  },
+);
+
+export default Flag;
