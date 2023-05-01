372e31a10126d04c4dbe856afefd67e373cd9b49
Tim Chen <timmyichen@users.noreply.github.com>
2023-04-30 18:10:19 -0700

    conditionally import dev router (#92)
    * conditionally import dev router
    
    * comment

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,8 +1,6 @@
 import { cleanEnv } from 'envalid';
 import { AsyncRouter } from 'express-async-router';
 
-import devRouter from 'server/api/dev';
-
 import authRouter from './auth';
 import userRouter from './users';
 
@@ -14,6 +12,8 @@ apiRouter.use('/users', userRouter);
 apiRouter.use('/auth', authRouter);
 
 if (env.isDev || env.isTest) {
+  // we can't import this router because it requires dev dependencies
+  const devRouter = require('./dev').default; // eslint-disable-line @typescript-eslint/no-var-requires
   // These should NEVER be loaded in production.
   apiRouter.use('/dev', devRouter);
 }
