cea07bb9ee27a29b1ec2f1efa133add3f2ff4f48
Ryan Mahara <ryanmahara@gmail.com>
2023-05-04 18:06:33 -0500

    discord_name should never be empty since we use discord auth (#88)
    * discord_name should never be empty since we use discord auth
    
    * revert some formatting that occured

--- server/test/utils.ts
+++ server/test/utils.ts
@@ -43,7 +43,7 @@ function makeUserObject({
   return {
     email: email ?? randEmail(),
     discord_user_id: discord_user_id ?? String(randNumber({ min: 1e16, max: 1e18 - 1 })),
-    discord_name: discord_name ?? randomEmptyChance(20, getRandomDiscordUserName()),
+    discord_name: discord_name ?? getRandomDiscordUserName(),
     bio: bio ?? randomEmptyChance(20, randQuote()),
     twitter_username: twitter_username ?? randomEmptyChance(20, randUserName()),
     linkedin_url: linkedin_url ?? randomEmptyChance(20, getRandomLinkedInURL()),
