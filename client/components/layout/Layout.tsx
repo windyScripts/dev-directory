07c55b031a6b1f252c1b5eee0c55b9891c556401
timmyichen <timmy.i.chen@gmail.com>
2023-05-06 02:38:20 -0700

    dark theme

--- client/components/layout/Layout.tsx
+++ client/components/layout/Layout.tsx
@@ -1,14 +1,20 @@
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import React from 'react';
 import Header from './Header';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const Layout: React.FC = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline enableColorScheme />
       <Header />
       <main>{children}</main>
-    </>
+    </ThemeProvider>
   );
 };
 
