305f0f419d08c8a507a199b3cde8845f36b583fe
Caleb Cohen <75268174+Caleb-Cohen@users.noreply.github.com>
2023-06-11 19:45:38 -0500

    Editing User Profile (#104)

---
+++ client/components/layout/UserForm.tsx
@@ -0,0 +1,112 @@
+import { Button, Container, TextField } from '@mui/material';
+import { Box } from '@mui/system';
+import axios from 'axios';
+import { useState } from 'react';
+
+import { useAuthDispatch } from 'client/contexts/auth';
+import { UserProfile } from 'server/types/User';
+
+interface Props {
+  user: UserProfile;
+  setUser: React.Dispatch<React.SetStateAction<UserProfile>>;
+}
+
+const UserForm:React.FC<Props> = ({ user, setUser }) => {
+  const authDispatch = useAuthDispatch();
+
+  const [userData, setUserData] = useState({
+    bio: user.bio || '',
+    twitter_username: user.twitter_username || '',
+    linkedin_url: user.linkedin_url || '',
+    github_username: user.github_username || '',
+    website: user.website || '',
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFormVisible, setIsFormVisible] = useState(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserData(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit:React.FormEventHandler<HTMLFormElement> = async event => {
+    event.preventDefault();
+    if (isLoading) {
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const userId = user.id;
+
+      await axios.patch(`/api/users/${userId}`, userData);
+      authDispatch({ type: 'SET_AUTHED_USER', user: { ...user, ...userData }});
+      setUser({
+        ...user,
+        ...userData,
+      });
+      setIsFormVisible(false);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (!isFormVisible) {
+    return null;
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <form onSubmit={handleSubmit}>
+        <Box display="flex" flexDirection="column" gap={2}>
+          <TextField
+            label="Bio"
+            name="bio"
+            multiline
+            rows={4}
+            value={userData.bio}
+            onChange={handleChange}
+            data-cy="bio-input"
+          />
+          <TextField
+            name="twitter_username"
+            label="Twitter Username"
+            value={userData.twitter_username}
+            onChange={handleChange}
+          />
+          <TextField
+            name="linkedin_url"
+            label="LinkedIn URL"
+            value={userData.linkedin_url}
+            onChange={handleChange}
+          />
+          <TextField
+            name="github_username"
+            label="GitHub Username"
+            value={userData.github_username}
+            onChange={handleChange}
+          />
+          <TextField
+            name="website"
+            label="Website"
+            value={userData.website}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={isLoading}
+            data-cy="update-button"
+          >
+            {isLoading ? 'Updating...' : 'Update'}
+          </Button>
+        </Box>
+      </form>
+    </Container>
+  );
+};
+
+export default UserForm;
