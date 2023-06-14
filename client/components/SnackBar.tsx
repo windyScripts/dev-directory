36077e0e616fd6ee1af95915e69e7facd5e087b0
uaravindshenoy <uaravindshenoy@gmail.com>
2023-06-14 18:01:07 +0530

    Success and error snackbar verified as working

---
+++ client/components/SnackBar.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@mui/material';
+import { useSnackbar, OptionsWithExtraProps } from 'notistack';
+import React from 'react';
+
+export function useAlerts(/* props: {  snackbarId: string; successMessage: string; errorMessage: string } */) {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const action = (snackbarId: string) => (
+    <>
+      <Button onClick={() => {
+        closeSnackbar(snackbarId);
+      }}>
+        Dismiss
+      </Button>
+    </>
+  );
+
+  const successOptions: OptionsWithExtraProps<'success'> = { action, autoHideDuration: 6000, variant: 'success' };
+  const errorOptions: OptionsWithExtraProps<'error'> = { action, autoHideDuration: 6000, variant: 'error' };
+  return (
+    [(message: string, props?: OptionsWithExtraProps<'success'>) => {
+      enqueueSnackbar(message, { ...successOptions, ...props });
+    },
+    (message: string, props?: OptionsWithExtraProps<'error'>) => {
+      enqueueSnackbar(message, { ...errorOptions, ...props });
+    }]
+  );
+}
