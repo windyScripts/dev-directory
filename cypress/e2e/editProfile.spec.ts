5f35ed33819a286f0e4ad00c8979de64092e9d81
timmyichen <timmy.i.chen@gmail.com>
2023-06-15 21:57:49 -0700

    fix cypress tests

--- cypress/e2e/editProfile.spec.ts
+++ cypress/e2e/editProfile.spec.ts
@@ -12,17 +12,17 @@ describe('Edit profile functions correctly', () => {
   });
 
   it('Prevents a user from editing other users profiles', () => {
-    cy.visit('http://localhost:3000/profile/2');
+    cy.visit('/profile/2');
     cy.get('[data-cy="edit-button"]').should('not.exist');
   });
 
   it('Allows a user the ability to edit their profile', () => {
-    cy.visit('http://localhost:3000/profile/1');
+    cy.visit('/profile/1');
     cy.get('[data-cy="edit-button"]').should('exist');
   });
 
   it('Editing profile works and updates the webpage', () => {
-    cy.visit('http://localhost:3000/profile/1');
+    cy.visit('/profile/1');
     cy.get('[data-cy="edit-button"]').click();
     cy.get('[data-cy="bio-input"]').clear().type('Tim prefers water with cereal');
     cy.get('[data-cy="update-button"]').click();
